d9d3596025432d984297e07231520f78a048cb4f
Carrington-dev <crn96m@gmail.com>
2024-07-24 10:24:51 +0200

    Added upto Main Urls and Fixed Serializer field order_number to read_only

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,8 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from orders.models import Order
 
 class OrderSerializer(ModelSerializer):
+    order_number = CharField(read_only=True)
+
     class Meta:
         model = Order
-        fields = [ "first_name", "last_name", "email", "order_number", ]
+        fields = [ 'id', "first_name", "last_name", "email", "order_number", ]
--- threadweb/urls.py
+++ threadweb/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import send_message
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/send_message', send_message, name="send_message"),
+    path('api/v1/', include("orders.urls")),
 ]
