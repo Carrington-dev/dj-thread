5e457e1be766a26577ac83f168c96a31f7375f2f
Carrington-dev <crn96m@gmail.com>
2024-07-24 10:18:41 +0200

    Added Default Router

---
+++ orders/urls.py
@@ -0,0 +1,7 @@
+from orders.views import OrderViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"orders", OrderViewSet, basename="order")
+
+urlpatterns = router.urls
